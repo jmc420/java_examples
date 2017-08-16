3e526c48deb2f463d6068f8136f6e3a8f429d4c2
facarvalho <facarvalho@paypal.com>
2017-08-16 17:22:44 -0500

    Turning it back to executable jar app Plus, making it work by adding web starter

--- spring-boot-arangodb/src/main/java/org/jamescowan/example/arangodb/ArangodbApplication.java
+++ spring-boot-arangodb/src/main/java/org/jamescowan/example/arangodb/ArangodbApplication.java
@@ -2,16 +2,9 @@ package org.jamescowan.example.arangodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArangodbApplication extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ArangodbApplication.class);
-    }
+public class ArangodbApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArangodbApplication.class, args);
